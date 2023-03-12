b44c977f0598dcaae3d7070869da17dd452d6452
aishyuu <ivanmatias1331@gmail.com>
2023-03-11 21:00:22 -0800

    first commit

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,10 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import YoutubeTranscript from "youtube-transcript";
+import { Configuration, OpenAIApi } from "openai";
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+export default async function handler(req, res) {
+  const body = req.body;
+  const videoLink = `The video link is ${body.video}`;
+  const transcript = await YoutubeTranscript.fetchTranscript(`${videoLink}`)
+  res.status(200).json({ video: transcript })
 }
